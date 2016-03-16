4f743672fdb5b171283772052904e9355ffae5b5
stella7 <stellamini88@gmail.com>
2016-03-16 13:21:24 -0400

    Add comments

--- src/CalConfidence.java
+++ src/CalConfidence.java
@@ -9,23 +9,24 @@ public class CalConfidence {
 
 		for(Integer keyFoo: graph.keySet()){
 			String calleeFoo = functions.get(keyFoo);
+			Set<Integer> callersFoo = graph.get(keyFoo);
+			int supportFoo = callersFoo.size();
+			
 			for(Integer keyBar: graph.keySet()){
 				String calleeBar = functions.get(keyBar);
+				
 				if(keyFoo != keyBar){
-					Set<Integer> callersFoo = graph.get(keyFoo);
-					//System.out.println(callersFoo);
+
 					Set<Integer> callersBar = graph.get(keyBar);
-					int supportFoo = callersFoo.size();
-					int supportBar = callersBar.size();
 					Set<Integer> tmp = new HashSet<Integer>();
 					tmp.addAll(callersFoo);
-					tmp.retainAll(callersBar);
-					//System.out.println(tmp);
+					tmp.retainAll(callersBar);					
 					int supportPair = tmp.size();
+					
 					if(supportPair >= support){
+						
 						double confidenceFoo = (double)supportPair/supportFoo;
-						//System.out.println(confidenceFoo);
-						double confidenceBar = (double)supportPair/supportBar;
+
 						if(confidenceFoo >= confidence){
 							Set<Integer> tmpFoo = new HashSet<Integer>();
 							tmpFoo.addAll(callersFoo);
@@ -41,28 +42,10 @@ public class CalConfidence {
 										System.out.printf("bug: %s in %s, pair: (%s, %s), support: %d, confidence: %.2f%%\n", 
 												calleeFoo, callerFoo, calleeBar, calleeFoo, supportPair, confidenceFoo*100);
 									}
-									//System.out.println("bug: "+calleeFoo +" in "+ callerFoo +" pair: ("
-									//		calleeFoo + ", " + calleeBar +"), support: "+supportFoo+" confidence: "%.2f%%\n",
-									//		, , , confidenceFoo);
 									
 								}
 							}
 						}
-					/*
-						if(confidenceBar >= confidence){
-							Set<Integer> tmpBar = new HashSet<Integer>();
-							tmpBar.addAll(callersBar);
-							
-							if(!tmpBar.isEmpty()){
-								for(Integer id:tmpBar){
-									String callerBar = functions.get(id);
-									System.out.printf("bug: %s in %s, pair: (%s, %s), support: %d, confidence: %.2f%%\n", 
-											calleeBar, callerBar, calleeBar, calleeFoo, supportPair, confidenceBar);
-								}
-							}
-						}
-						*/
-						
 						
 					}					
 				}
--- src/Main.java
+++ src/Main.java
@@ -9,46 +9,48 @@ import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 public class Main {
+	// Set default value for T_SUPPORT and T_CONFIDENCE
 	public static int T_SUPPORT = 3;
 	private static double T_CONFIDENCE = 65;
+	// functions<functionID, functionName>: A HashMap with functionID as key and functionName as value
 	static HashMap<Integer, String> functions = new HashMap<Integer, String>();
+	// graph<calleeId, Set<callerID>>: A HashMap with calleeID as key and a HashSet of callerID as value
 	static HashMap<Integer, HashSet<Integer>> graph = new HashMap<Integer, HashSet<Integer>>();
 	
 	public static void main(String[] args){
 		int support = T_SUPPORT;
 		double confidence = T_CONFIDENCE/100;
-
 		String fileName = "";
+		
 		if (args.length > 0){
-			int i = 0;
-			while(i < args.length){
-				switch(i){
-				case 0:
-					fileName = args[0];
-					break;
-				case 1:
-					support = Integer.parseInt(args[1]);
-					break;
-				case 2:
-					confidence = Double.parseDouble(args[2])/100;
-					break;
-				default:
-					System.out.println("Error: Wrong Number of Input Arguments");
-					System.exit(-1);
-				}
-				i++;
+			fileName = args[0];
+			
+			switch(args.length){
+			case 1:
+				break;
+			case 3:
+				support = Integer.parseInt(args[1]);
+				confidence = Double.parseDouble(args[2])/100;
+				break;
+			default:
+				System.out.println("Error: Wrong Number of Input Arguments");
+				System.exit(-1);
 			}
+
 		}else {
-			System.out.println("Error: Arument needs to contain CallGraph file name");
+			System.out.println("Error: Arument needs to contain at least callgraph file name");
 			System.exit(-1);
 		}
-		//System.out.println(confidence);
+
 		CallGraph callGraph = new CallGraph();
 		Parse(fileName,callGraph);
 		CalConfidence con = new CalConfidence();
 		con.PairConfidence(graph, functions, support, confidence);
 	}//end of main method
 	
+	// This method parses each line of the input callgraph file
+	// Store each function's name(String) and ID(hashCode) into functions
+	// Map each calleeID and its callersID and stored them into graph 
 	static void Parse(String fileName, CallGraph callgraph){
 		Pattern nodePattern = Pattern.compile("Call graph node for function: '(.*?)'<<.*>>  #uses=(\\d*).*$");
 		Pattern functionPattern = Pattern.compile("CS<(.*)> calls function '(.*?)'.*$");
@@ -60,6 +62,7 @@ public class Main {
 			BufferedReader br = new BufferedReader(new FileReader(fileName));
 			String currentLine = null;
 			currentLine = br.readLine();
+			
 			//skip the <<null function>> node
 			while(!currentLine.isEmpty()){
 				currentLine = br.readLine();
@@ -69,36 +72,25 @@ public class Main {
 				Matcher nodeMacher = nodePattern.matcher(currentLine);
 				if(nodeMacher.find()){
 					callerName = nodeMacher.group(1);
-					//Function caller = new Function(callerName.hashCode(),callerName);
-					//functions.add(caller);
 					callgraph.addToFunctionSet(callerName);
-					//System.out.println(caller.getId()+caller.getName());
 				}
 				Matcher functionMatcher = functionPattern.matcher(currentLine);
 				if(functionMatcher.find()){
 					calleeName = functionMatcher.group(2);
-					//Function callee = new Function(calleeName.hashCode(),calleeName);
-					//functions.add(callee);
 					callgraph.addToFunctionSet(calleeName);
 					callgraph.createGraph(calleeName, callerName);
-					//System.out.println(callee.getId()+callee.getName());
-
 				}
 			}//end of while
+			
 			br.close();
+			
 		}catch(IOException e){
 			e.printStackTrace();
 			System.exit(-1);
 		}
 		
-		functions = callgraph.getFunctions();
-/*		
-		for( Map.Entry<Integer, String> entry:functions.entrySet()){
-			System.out.println("Key = " + entry.getKey() + ", Value = " + entry.getValue());
-		}
-*/		
+		functions = callgraph.getFunctions();		
 		graph = callgraph.getGraph();
-		//System.out.println(graph);
 		
 	}//end of parse
 	
