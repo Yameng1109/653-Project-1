446252918afe482c9ce8ab75ea001c0005b351db
Yuzhou Wang <wangyuzhou777@gmail.com>
2016-03-20 12:29:47 -0400

    modified minor

--- src2/CalConfidence.java
+++ src2/CalConfidence.java
@@ -15,33 +15,36 @@ public class CalConfidence {
 		 * find bug locations of Foo
 		 */
 		for(String calleeFoo: graph.keySet()){
-			Set<String> callersFoo = graph.get(calleeFoo);
-			int supportFoo = callersFoo.size();
+			Set<String> callersFoo, callersBar, tmp, tmpFoo;
+			int supportFoo, supportPair, compare;
+			double confidenceFoo;
+			
+			callersFoo = graph.get(calleeFoo);
+			tmp = new HashSet<String>();	
+			tmpFoo = new HashSet<String>();			
+			supportFoo = callersFoo.size();
 			
 			for(String calleeBar: graph.keySet()){				
-				if(calleeFoo != calleeBar){
+				if(!calleeFoo.equals(calleeBar)){
 					/* Compare caller sets of Foo and Bar
 					 * The number of common nodes is the pair(Foo,Bar) support
 					 */
-					Set<String> callersBar = graph.get(calleeBar);
-					Set<String> tmp = new HashSet<String>();
+					callersBar = graph.get(calleeBar);
 					tmp.addAll(callersFoo);
-					
+
 					// Set the common nodes
 					tmp.retainAll(callersBar);					
-					int supportPair = tmp.size();
+					supportPair = tmp.size();
 					
 					if(supportPair >= support){
-						double confidenceFoo = (double)supportPair/supportFoo;// Confidence of Foo on pair(Foo,Bar)
-
+						confidenceFoo = (double)supportPair/supportFoo;// Confidence of Foo on pair(Foo,Bar)
 						if(confidenceFoo >= confidence){
 							// The nodes not in common set are labeled as bugs
-							Set<String> tmpFoo = new HashSet<String>();
 							tmpFoo.addAll(callersFoo);
 							tmpFoo.removeAll(tmp);		//Set of nodes not in common set
 							if(!tmpFoo.isEmpty()){
 								for(String callerFoo:tmpFoo){
-									int compare = calleeFoo.compareTo(calleeBar);		// Sort the pair functions' names
+									compare = calleeFoo.compareTo(calleeBar);		// Sort the pair functions' names
 									if(compare < 0){
 										System.out.printf("bug: %s in %s, pair: (%s, %s), support: %d, confidence: %.2f%%\n", 
 												calleeFoo, callerFoo, calleeFoo, calleeBar, supportPair, confidenceFoo*100);
